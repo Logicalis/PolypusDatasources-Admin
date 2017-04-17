08fb65f9b7ba9927a7a180dda57a48769fac0b23
BrOrlandi <brorlandi@gmail.com>
2017-04-16 21:11:40 -0300

    Change to use react-bigtext

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import {Grid, Col, Panel} from  'react-bootstrap';
 
-import BigText from 'big-text.js';
+import BigText from 'react-bigtext';
 
 import AdapterService from '../service/AdapterService';
 import DataSourceService from '../service/DataSourceService';
@@ -31,12 +31,6 @@ class Home extends Component {
         });
     }
 
-    componentDidUpdate() {
-        BigText(this.refs.bigText1);
-        BigText(this.refs.bigText2);
-        // BigText(this.refs.bigText3);
-    }
-
     navigateTo = (url) =>{
         this.props.history.push(url);      
     }    
@@ -48,25 +42,25 @@ class Home extends Component {
                     <Col xs={12} md={4}>
                         <Panel style={{cursor: 'pointer'}} onClick={()=>{this.navigateTo('/adapters')}}>
                             <h2 style={{textAlign: 'center'}}>Adapters</h2>
-                            <div style={{height: '100px'}}>
-                                <span ref="bigText1">{this.state.adaptersCount}</span>
-                            </div>
+                            <BigText options={{height: '100px', delay:100}}>
+                                {this.state.adaptersCount}
+                            </BigText>
                         </Panel>
                     </Col>
                     <Col xs={12} md={4}>
                         <Panel style={{cursor: 'pointer'}} onClick={()=>{this.navigateTo('/datasources')}}>
                             <h2 style={{textAlign: 'center'}}>DataSources</h2>
-                            <div style={{height: '100px'}}>
-                                <span ref="bigText2">{this.state.datasourcesCount}</span>
-                            </div>
+                            <BigText options={{height: '100px', delay:100}}>
+                                {this.state.datasourcesCount}
+                            </BigText>
                         </Panel>
                     </Col>
                     {/*<Col xs={12} md={4}>
                         <Panel style={{cursor: 'pointer'}} onClick={()=>{this.navigateTo('/queries')}}>
                             <h2 style={{textAlign: 'center'}}>Queries</h2>
-                            <div style={{height: '100px'}}>
-                                <span ref="bigText3">{this.state.queriesCount}</span>
-                            </div>
+                            <BigText options={{height: '100px', delay:100}}>
+                                {this.state.queriesCount}
+                            </BigText>
                         </Panel>
                     </Col>*/}
                 </Grid>
